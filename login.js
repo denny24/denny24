7128dc553af82490fc6b6a5493edab7649616a0c
deeny24 <denibelung1234@gmail.com>
2023-06-19 15:44:57 +0700

    sdfsd

--- login.js
+++ login.js
@@ -1,7 +1,3 @@
-function toggleDarkMode() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
-}
 function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -14,6 +10,9 @@ function validateLogin() {
     });
 
     if (user) {
+        // Setelah login berhasil
+        var username = user.username; // Ganti dengan username yang sebenarnya
+        localStorage.setItem("username", username);
         var popupContent = document.getElementById("popup-content");
         popupContent.innerHTML = "<h2>Login berhasil!</h2><p>Selamat datang, " + user.username + "!</p><button class='close-btn' onclick='closePopupAndRedirect()'>Close</button>";
         popup.style.display = "block"; // Tampilkan pop-up saat login berhasil
@@ -23,31 +22,40 @@ function validateLogin() {
 }
 
 function registerAccount() {
-    var regUsername = document.getElementById("reg-username").value;
-    var regPassword = document.getElementById("reg-password").value;
+  var regUsername = document.getElementById("reg-username").value;
+  var regPassword = document.getElementById("reg-password").value;
 
-    // Lakukan validasi sesuai kebutuhan
-    if (regUsername && regPassword) {
-        var registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) || [];
-        var existingUser = registeredUsers.find(function (user) {
-            return user.username === regUsername;
-        });
+  // Lakukan validasi sesuai kebutuhan
+  if (regUsername && regPassword) {
+    var registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+    var existingUser = registeredUsers.find(function (user) {
+      return user.username === regUsername;
+    });
 
-        if (existingUser) {
-            alert("Username sudah digunakan. Silakan pilih username lain.");
-        } else {
-            registeredUsers.push({ username: regUsername, password: regPassword });
-            localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
-
-            var popup = document.getElementById("popup");
-            var popupContent = document.getElementById("popup-content");
-            popupContent.innerHTML = "<h2>Akun berhasil terdaftar!</h2><p>Silakan login dengan akun baru Anda.</p><button class='close-btn' onclick='closePopupAndLogin()'>Close</button>";
-            popup.style.display = "block"; // Tampilkan pop-up setelah registrasi berhasil
-        }
+    if (existingUser) {
+      alert("Username sudah digunakan. Silakan pilih username lain.");
     } else {
-        alert("Mohon isi username dan password dengan benar.");
+      // Buat objek akun baru dengan saldo awal
+      var newUser = {
+        username: regUsername,
+        password: regPassword,
+        saldo: 1000000,
+      };
+
+      registeredUsers.push(newUser);
+      localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+
+      // Tampilkan pop-up selamat
+      var popup = document.getElementById("popup");
+      var popupContent = document.getElementById("popup-content");
+      popupContent.innerHTML = "<h2>Akun berhasil terdaftar!</h2><p>Selamat Datang, " + newUser.username + "! <br> Anda mendapatkan saldo sebesar Rp1.000.000.</p><button class='close-btn' onclick='closePopupAndLogin()'>Close</button>";
+      popup.style.display = "block"; // Tampilkan pop-up setelah registrasi berhasil
     }
+  } else {
+    alert("Mohon isi username dan password dengan benar.");
+  }
 }
+
 
 
 function closePopup() {
@@ -58,7 +66,7 @@ function closePopup() {
 function closePopupAndRedirect() {
     var popup = document.getElementById("popup");
     popup.style.display = "none"; // Sembunyikan pop-up saat tombol close ditekan
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"; // Buka halaman YouTube setelah popup ditutup
+    window.location.href = "halaman/page.html";
 }
 
 function closePopupAndLogin() {
@@ -99,3 +107,12 @@ function forgotPassword() {
         alert("Mohon masukkan username Anda terlebih dahulu.");
     }
 }
+
+var passwordInput = document.getElementById("password");
+
+passwordInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter" || event.key === "NumpadEnter") {
+    event.preventDefault(); // Mencegah submit form bawaan
+    validateLogin(); // Panggil fungsi untuk validasi login
+  }
+});
